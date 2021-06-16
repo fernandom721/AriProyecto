f01f39bfffe6fe49947ba5258524c2a4463e451b
Fernando Martinez <fmartinez@aseinfo.com.sv>
2021-06-16 16:47:15 -0600

    origen de la info en el main

--- AriProyecto/Program.cs
+++ AriProyecto/Program.cs
@@ -1,12 +1,46 @@
-﻿using System;
+﻿using AriProyecto.Manager;
+using AriProyecto.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
 
 namespace AriProyecto
 {
+    //URL DE DONDE SAQUE LAS COSAS https://medium.com/@mmoshikoo/jwt-authentication-using-c-54e0c71f21b0
+
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            IAuthContainerModel model = (IAuthContainerModel)GetJWTContainerModel("Fernando Martinez", "00076617@uca.edu.sv");
+            IAuthService authService = new JWTService(model.SecretKey);
+
+            string token = authService.GenerateToken(model);
+
+            if (!authService.IsTokenValid(token))
+                throw new UnauthorizedAccessException();
+            else
+            {
+                List<Claim> claims = authService.GetTokenClaims(token).ToList();
+
+                Console.WriteLine(claims.FirstOrDefault(e => e.Type.Equals(ClaimTypes.Name)).Value);
+                Console.WriteLine(claims.FirstOrDefault(e => e.Type.Equals(ClaimTypes.Email)).Value);
+            }
         }
+
+        #region Private Methods
+        private static JWTContainerModel GetJWTContainerModel(string name, string email)
+        {
+            return new JWTContainerModel()
+            {
+                Claims = new Claim[]
+                {
+                    new Claim(ClaimTypes.Name, name),
+                    new Claim(ClaimTypes.Email, email)
+                }
+            };
+        }
+        #endregion
     }
 }
