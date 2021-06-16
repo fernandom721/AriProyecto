2c89d7f3acf7a8fa513bb147044af3904a936c49
Fernando Martinez <00076617@uca.edu.sv>
2021-06-16 16:44:10 -0600

    initial commit

---
+++ AriProyecto/Model/JWTContainerModel.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.IdentityModel.Tokens;
+using System;
+using System.Collections.Generic;
+using System.Security.Claims;
+using System.Text;
+
+namespace AriProyecto.Model
+{
+    public class JWTContainerModel
+    {
+
+        public int ExpireMinutes { get; set; } = 10080;
+        public string SecretKey { get; set; } = "QK8JLkjfp2387tjvl1749fjHHavjltr87hH99y";
+        public string SecurityAlgorithm { get; set; } = SecurityAlgorithms.HmacSha256Signature;
+
+        public Claim[] Claims { get; set; }
+    }
+}
